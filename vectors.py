67a571e97c707abebb83eff914c44bcb861c31c1
adk7 <rakesh.adhikesavan@nielsen.com>
2017-12-20 16:30:36 -0600

    Added Code

--- vectors.py
+++ vectors.py
@@ -1,3 +1,7 @@
+from __future__ import division
+import math
+
+
 class LengthException(Exception):
 	pass
 
@@ -37,11 +41,22 @@ class Vector(object):
 		return Vector(vector_diff)
 
 	def __mul__(self, other):
-		if isinstance(other, int):
+		if isinstance(other, (int, float)):
 			vector_scaled = [x * other for x in self.coordinates]
 			return Vector(vector_scaled)
+		elif isinstance(other, Vector):
+			dot_prod = math.sqrt(sum([x * x + y * y for x, y in zip(self.coordinates, other.coordinates)]))
+			return dot_prod
 		else:
-			raise TypeError("Can only multipy with scalar integer")
+			raise TypeError("Can only multiply vector with a scalar integer")
 
+	def magnitude(self):
+		total = 0
+		for c in self.coordinates:
+			total += c * c
+		return math.sqrt(total)
 
-
+	def normalize(self):
+		mag = self.magnitude()
+		normal_vector = self * (1/mag)
+		return normal_vector
